81b64b47dca1c8ea6d3bf90525b910d416fca474
aliargun <argun.ali@gmail.com>
2024-12-16 09:42:14 +0300

    Add Jest configuration for testing setup

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,15 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/tests/**/*.test.ts'],
-  moduleFileExtensions: ['ts', 'js'],
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
   transform: {
     '^.+\\.ts$': 'ts-jest'
   },
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json'
-    }
-  }
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts']
 };
