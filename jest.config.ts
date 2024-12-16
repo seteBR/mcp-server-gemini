7f2577902c90563a5ae44a6c4b17062dc37baf39
aliargun <argun.ali@gmail.com>
2024-12-16 09:45:15 +0300

    Add Jest configuration file

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/__tests__/'],
+  setupFilesAfterEnv: ['<rootDir>/src/__tests__/setup.ts']
+};
